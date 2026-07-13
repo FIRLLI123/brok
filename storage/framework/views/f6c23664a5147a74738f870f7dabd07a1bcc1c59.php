d30ad0132610881ba1c22b197fac3384c5703afb
FIRLLI123 <firlliantonizi@gmail.com>
2026-07-13 16:19:52 +0700

    profile fix

---
+++ storage/framework/views/f6c23664a5147a74738f870f7dabd07a1bcc1c59.php
@@ -0,0 +1,11 @@
+<button
+    <?php echo e($attributes->merge([
+        'type' => 'submit',
+        'class' => 'inline-flex items-center px-4 py-2 font-semibold text-xs text-white uppercase tracking-widest rounded-md transition ease-in-out duration-150',
+    ])); ?>
+
+    style="background: linear-gradient(90deg, #6366f1 0%, #a21caf 100%); border: none; box-shadow: 0 2px 8px 0 rgba(99,102,241,0.10);"
+>
+    <?php echo e($slot); ?>
+
+</button> <?php /**PATH C:\laragon\www\Brock\resources\views/components/primary-button.blade.php ENDPATH**/ ?>
